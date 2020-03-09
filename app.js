f86d79ccc021f560bfd1377ca4c2a31f2c86e011
el-papi-chiludo <d7distrito@gmail.com>
2020-03-09 14:30:47 -0600

    cambio de puerto

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const dataJSON = JSON.parse(data);
 const express = require("express");
 const app = express();
 //console.log(dataJSON[0]['materias_semestres'][0]['1']['materias'][1]);
-console.log(dataJSON[0]["materias_info"][0]["CR"]);
+//console.log(dataJSON[0]["materias_info"][0]["CR"]);
 
-const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.get("/api/v1/materiasInfo", (req, res) => {
   res.status(200).json({
